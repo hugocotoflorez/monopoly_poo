2492156658b2388554702e755fa9f53e53b20dd8
hugocotoflorez <hugocoto100305@gmail.com>
2024-09-18 13:19:25 +0200

    addign name convention?

--- monopoly/Termctl.java
+++ monopoly/Termctl.java
@@ -2,21 +2,21 @@ package monopoly;
 
 public class Termctl {
     // colors
-    public static final int black = 0;
-    public static final int red = 1;
-    public static final int green = 2;
-    public static final int yellow = 3;
-    public static final int blue = 4;
-    public static final int magenta = 5;
-    public static final int cyan = 6;
-    public static final int white = 7;
+    public static final int Black = 0;
+    public static final int Red = 1;
+    public static final int Green = 2;
+    public static final int Yellow = 3;
+    public static final int Blue = 4;
+    public static final int Magenta = 5;
+    public static final int Cyan = 6;
+    public static final int White = 7;
     // styles
-    public static final int normal = 1 << 0;
-    public static final int bold = 1 << 1;
-    public static final int italic = 1 << 2;
-    public static final int faint = 1 << 3;
-    public static final int underline = 1 << 4;
-    public static final int inverse = 1 << 5;
+    public static final int Normal = 1 << 0;
+    public static final int Bold = 1 << 1;
+    public static final int Italic = 1 << 2;
+    public static final int Faint = 1 << 3;
+    public static final int Underline = 1 << 4;
+    public static final int Inverse = 1 << 5;
 
     public void apply(int color, int style)
     {
