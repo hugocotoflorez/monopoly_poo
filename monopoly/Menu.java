225fa8eedc27ff784c6afefbb0f42440f1348f1d
Guilluuu <guillegafc@gmail.com>
2024-10-01 17:50:40 +0200

    cambios menu

--- monopoly/Menu.java
+++ monopoly/Menu.java
@@ -81,7 +81,7 @@ public class Menu {
                 System.out.println(tablero);
                 break;
             case "acabar partida":
-                this.jugadores.get(turno).acabarPartida();
+                acabarPartida();
                 break;
 
             default:
