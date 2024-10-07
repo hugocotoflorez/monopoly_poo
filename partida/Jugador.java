098d7a6d1a22bb15f9e95e4f10d058b5a9cafc17
ma-r1 <redondelana@gmail.com>
2024-10-07 21:00:05 +0200

    encarcelar arreglado

--- partida/Jugador.java
+++ partida/Jugador.java
@@ -146,9 +146,8 @@ public class Jugador {
      * como parámetro).
      */
     public void encarcelar(ArrayList<ArrayList<Casilla>> pos) {
-
         this.avatar.setLugar(pos.get(1).get(0));
-
+        this.enCarcel = true;
     }
 
     public boolean esBanca() {
