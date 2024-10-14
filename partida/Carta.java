632e6d901f5d2d78a67bd2f040b851717bc2249a
hugocotoflorez <hugocoto100305@gmail.com>
2024-10-14 13:53:50 +0200

    carta faltan las funciones especificas

--- partida/Carta.java
+++ partida/Carta.java
@@ -128,39 +128,77 @@ public class Carta {
     }
 
     private void accSuerte1(Avatar av) {
+        /**
+         *Ve al Transportes1 y coge un avión. Si pasas por la casilla de Salida, cobra la cantidad habitual.
+         */
     }
 
     private void accSuerte2(Avatar av) {
+        /**
+         *Decides hacer un viaje de placer. Avanza hasta Solar15.
+         */
     }
 
     private void accSuerte3(Avatar av) {
+        /**
+         *Vendes tu billete de avión para Solar171 en una subasta por Internet. Cobra 500000€
+         */
     }
 
     private void accSuerte4(Avatar av) {
+        /**
+         *Ve a Solar3. Si pasas por la casilla de Salida, cobra la cantidad habitual.
+         */
     }
 
     private void accSuerte5(Avatar av) {
+        /**
+         *Los acreedores te persiguen por impago. Ve a la Cárcel. Ve directamente sin pasar por la casilla de Salida
+y sin cobrar la cantidad habitual
+         */
     }
 
     private void accSuerte6(Avatar av) {
+        /**
+         *¡Has ganado el bote de la lotería! Recibe 1000000€
+         */
     }
 
     private void accComm1(Avatar av) {
+        /**
+         *Paga 500000€ por un fin de semana en un balneario de 5 estrellas
+         */
     }
 
     private void accComm2(Avatar av) {
+        /**
+         *Te investigan por fraude de identidad. Ve a la Cárcel. Ve directamente sin pasar por la casilla de Salida y
+sin cobrar la cantidad habitual
+         */
     }
 
     private void accComm3(Avatar av) {
+        /**
+         *Colócate en la casilla de Salida. Cobra la cantidad habitual.
+         */
     }
 
     private void accComm4(Avatar av) {
+        /**
+         *Tu compañía de Internet obtiene beneficios. Recibe 2000000€
+         */
     }
 
     private void accComm5(Avatar av) {
+        /**
+         *Paga 1000000€ por invitar a todos tus amigos a un viaje a Solar142.
+         */
     }
 
     private void accComm6(Avatar av) {
+        /**
+         *Alquilas a tus compañeros una villa en Solar7 durante una semana. Paga 200000€ a cada jugador3
+         */
     }
 
 }
