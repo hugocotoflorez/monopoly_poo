6c3b1e09c843946e7cbd2bccfc7b730f1395d363
ma-r1 <redondelana@gmail.com>
2024-12-01 16:10:18 +0100

    crear archivos exception

---
+++ monopoly/MonopolyException/AccionException/AccionException.java
@@ -0,0 +1,9 @@
+package monopoly.MonopolyException.AccionException;
+
+import monopoly.MonopolyException.MonopolyException;
+
+public abstract class AccionException extends MonopolyException {
+    public AccionException(String message){
+        super(message);
+    }
+}
---
+++ monopoly/MonopolyException/PropiedadException/PropiedadException.java
@@ -0,0 +1,9 @@
+package monopoly.MonopolyException.PropiedadException;
+
+import monopoly.MonopolyException.MonopolyException;
+
+public abstract class PropiedadException extends MonopolyException{
+    public PropiedadException(String message){
+        super(message);
+    }
+}
